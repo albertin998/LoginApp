fc3c9c7be5461dae6371d993c9250f07ea381c2b
Alberto carvalho <albertocarvalho998@gmail.com>
2025-04-14 21:23:29 -0300

    segundo commit

--- app/src/main/java/ActivityCadastroUsuario.java
+++ app/src/main/java/ActivityCadastroUsuario.java
@@ -1,32 +1,6 @@
-public class ActivityCadastroUsuario extends AppCompatActivity {
-    private EditText editNome, editEmail, editSenha, editConfirmarSenha;
-    private Button btnSalvarCadastro;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cadastro_usuario);
-
-        editNome = findViewById(R.id.editNome);
-        editEmail = findViewById(R.id.editEmail);
-        editSenha = findViewById(R.id.editSenha);
-        editConfirmarSenha = findViewById(R.id.editConfirmarSenha);
-        btnSalvarCadastro = findViewById(R.id.btnSalvarCadastro);
-
-        btnSalvarCadastro.setOnClickListener(v -> {
-            if (!editSenha.getText().toString().equals(editConfirmarSenha.getText().toString())) {
-                abrirTelaErro("As senhas não coincidem!");
-            } else {
-
-                Toast.makeText(this, "Usuário cadastrado!", Toast.LENGTH_SHORT).show();
-                finish();
-            }
-        });
+public class ActivityCadastroUsuario {
     }
 
-    private void abrirTelaErro(String mensagem) {
-        Intent intent = new Intent(this, ActivityErro.class);
-        intent.putExtra("mensagem", mensagem);
-        startActivity(intent);
-    }
-}
+
+
+
